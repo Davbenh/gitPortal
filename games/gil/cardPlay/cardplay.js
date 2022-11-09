86f84262a8faceb57ff6302d814bc909f48339b9
gil <gilcohavi26@gmail.com>
2022-11-09 13:53:17 +0200

    last.v

--- games/gil/cardPlay/cardplay.js
+++ games/gil/cardPlay/cardplay.js
@@ -1,9 +1,18 @@
 const cards = document.querySelectorAll('.memory-card');
 const gameBoard = document.getElementById('gameBoard')
 const stopwatchSpan = document.getElementById('stopwatch');
+var list = document.getElementById('users');
+var bestTime = document.getElementById('bestTime');
+var usernameSpan = document.getElementById('usernameSpan');
+let counter = document.querySelector(".moves");
+var scoreElt = document.querySelector("#score");
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+
+
+let userName
 let hasFlippedCard = false;
 let lockBoard = false;
 let startTime = false;
@@ -12,9 +21,50 @@ let flippedCouplesCount = 0;
 let hh = 0;
 let mm = 0;
 let ss = 0;
-let score =0 ;
+var score =0 ;
 
 let TOTAL_COUPLES_COUNT = 6;
+
+// Check if a username is stored in localStorage
+if (localStorage.getItem('username') === null) {
+  var usernameInput = prompt('Enter your name!');
+
+  if (usernameInput === '' || usernameInput === null) {
+    localStorage.setItem(
+      'username',
+      `User${Math.floor(
+        Math.random() * (Math.floor(99999) - Math.ceil(10000) + 1) + 10000
+      )}`
+    );
+    usernameSpan.innerText = `Hello ${localStorage.getItem('username')}!`;
+  } else {
+    localStorage.setItem('username', usernameInput);
+    usernameSpan.innerText = `Hello ${localStorage.getItem('username')}!`;
+  }
+} else {
+  usernameSpan.innerText = `Hello ${localStorage.getItem('username')}!`;
+}
+
+
+
+function changeUser() {
+  var usernameInput = prompt('Enter your name!');
+
+  if (usernameInput === '' || usernameInput === null) {
+    localStorage.setItem(
+      'username',
+      `User${Math.floor(
+        Math.random() * (Math.floor(99999) - Math.ceil(10000) + 1) + 10000
+      )}`
+    );
+    usernameSpan.innerText = `Hello ${localStorage.getItem('username')}!`;
+    } else {
+     localStorage.setItem('username', usernameInput);
+    usernameSpan.innerText = `Hello ${localStorage.getItem('username')}!`;
+   }
+ }
+
+
 
 function flipCard() {
   if (lockBoard) return;
@@ -43,10 +93,12 @@ function checkForMatch() {
     flippedCouplesCount++;
     score = score + 10 ;
     localStorage.setItem("score", score);
+    scoreElt.innerHTML = score;
 
    disableCards();
    if (TOTAL_COUPLES_COUNT === flippedCouplesCount) {
     startTime = false;
+    
    }
    return;
  }
@@ -61,16 +113,20 @@ function disableCards() {
  resetBoard();
 }
 
+
+
 function unflipCards() {
   lockBoard = true;
 
  setTimeout(() => {
    firstCard.classList.remove('flip');
    secondCard.classList.remove('flip');
-
    lockBoard = false;
    resetBoard();
- }, 1500);
+  }, 1500);
+  if (score > 0) {
+    score -= 2;
+  }
 }
 
 function resetBoard() {
@@ -118,4 +174,11 @@ function stopwatchFunc() {
 
   stopwatchSpan.innerText = `${hh}:${mm}:${ss}`;
   setTimeout('stopwatchFunc()', 1000);
+}
+function addUser(){
+  var username =document.getElementById('username').value;
+
+  var entry = document.createElement('li');
+  entry.appendChild(document.createTextNode(username + '' + email));
+  list.appendChild(entry);
 }
